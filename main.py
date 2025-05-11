16346269b0d7dd08461cbd9e3bc1550f18ad4243
zerogravitas <jlwoods83@gmail.com>
2025-05-11 19:32:24 +1000

    CH3, L1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_character_tally
+from stats import get_ordered_character_tally
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -12,8 +13,32 @@ def get_book_text(path_to_file):
 #    print(f"{num_words} words found in the document")
 
 #get tally of characters
+#def main():
+#    character_tally = get_character_tally(get_book_text("books/frankenstein.txt"))
+#    print(character_tally)
+
+#get ordered tally of characters
+#def main():
+#    ordered_character_tally = get_ordered_character_tally(get_character_tally(get_book_text("books/frankenstein.txt")))
+#    print(ordered_character_tally)
+
 def main():
-    character_tally = get_character_tally(get_book_text("books/frankenstein.txt"))
-    print(character_tally)
+    #get number of words and ordered tally
+    num_words = len(get_num_words(get_book_text("books/frankenstein.txt")))
+    ordered_character_tally = get_ordered_character_tally(get_character_tally(get_book_text("books/frankenstein.txt")))
+
+    #print header
+    print("""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...""")
+    #print word count
+    print(f"""----------- Word Count ----------
+Found {num_words} total words""")
+    #print ordered list
+    print("--------- Character Count -------")
+    for dictionary in ordered_character_tally:
+        print(f"{dictionary["char"]}: {dictionary["num"]}")
+    #print end
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,18 @@ def get_character_tally(book_text):
     for char in lowered_book_text:
         character_tally[char] += 1
     return character_tally
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_ordered_character_tally(character_tally):
+    # init list
+    ordered_character_tally = []
+    #ccreate list of dictionaries
+    for char in character_tally:
+        if char.isalpha() == True:
+            ordered_character_tally.append({"char": char, "num": character_tally[char]})
+    #order list of dictionaries        
+    ordered_character_tally.sort(reverse=True, key=sort_on) # I do not understand how tf sort_on actually works here but fuckit.
+
+    return ordered_character_tally
